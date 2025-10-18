d04b6498cd685c2dc31e756d2cd2c85fe2c15fed
dark python <rishabhpatel366@gmail.com>
2025-10-18 21:50:42 +0530

    Implement Firebase authentication and protected routes

--- client/src/pages/firebase.js
+++ client/src/pages/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp} from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwmqG3Lw-SVwks5HV-PfeE8P_e_oBdon8",
-  authDomain: "hackapp-1dc5d.firebaseapp.com",
-  databaseURL: "https://hackapp-1dc5d-default-rtdb.firebaseio.com",
-  projectId: "hackapp-1dc5d",
-  storageBucket: "hackapp-1dc5d.firebasestorage.app",
-  messagingSenderId: "485679819428",
-  appId: "1:485679819428:web:1fd11f6eaa059770041431",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  databaseURL: import.meta.env.VITE_databaseURL,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
   //measurementId: "G-16X6CQBT7H"
 };
 
